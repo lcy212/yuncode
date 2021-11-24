2c93fbf4d686adf9ee30f73d415b3239bf427a0f
lcy212 <chaeyoon59@gmail.com>
2021-11-25 00:52:51 +0900

    1124

--- b_code/b_step_03/js/src/c_01_dom_sample_01.js
+++ b_code/b_step_03/js/src/c_01_dom_sample_01.js
@@ -9,10 +9,21 @@
 // 1-1. 버튼 : .navbar-toggler -> var navToggleBtn = document.querySelector('.navbar-toggler');
 // 1-2/ 클릭 : 선택자 클릭(addEventListener) - navToggleBtn.addEventListener('click',function(event){});
 // 1-3. 메뉴 : id="navbarSupportedContent" -> document.querySelector('#navbarSupportedContent');
+// 1-4. 나타나기 : html.classname="action" -> classname 추가, (none -> block)
+// 1-5. 기능 수행 전 확인사항 : action의 유무 - classList.contains('action')
+//                              해당버튼의 고유기능 해제 - event.preventDefault()
 
 var navToggleBtn = document.querySelector('.navbar-toggler');
 var navbarSupportedContent = document.querySelector('#navbarSupportedContent');
 
 navToggleBtn.addEventListener('click',function(event){
-  // 1-4 수행
+  event.preventDefault(); // navToggleBtn이 기존에 갖고있던 기능을 해제함
+  var checkClassName = 'action' // class이름을 일괄로 변경할 수 있게 변수 생성
+  var isAction = navbarSupportedContent.classList.contains(checkClassName); // action이 있는지 확인하여 true, false로 확인
+  if(!isAction){ // action이 없으면 넣고, 있으면 빼라는 조건문
+    navbarSupportedContent.classList.add(checkClassName);
+  }else{
+    navbarSupportedContent.classList.remove(checkClassName);
+  }
+
 });
---
+++ b_code/b_step_03/js/src/c_02_dom_make_navi_02.js
@@ -0,0 +1,35 @@
+// c_02_dom_make_navi_02.js
+
+// 버튼클릭하면 하위메뉴가 나타거나 사라지게 만들기
+
+
+// 변수 -------------------------------------------------------
+var dropBtnSelector = document.querySelector('.drop_btn');
+var dropBtn = dropBtnSelector.querySelector('button');
+var subMenu = document.querySelector('.sub_menu');
+
+// 전달받은 요소의 모든 CSS 속성값을 담은 객체를 회신
+var subMenuStyle = getComputedStyle(subMenu);
+// console.log(subMenuStyle.display);
+
+
+var displayStateFn = function(selector){
+  // 선택자의 display 상태값을 기본 html에서 가져오지만 없다면 css 기본값에서 가져오기
+  return selector.style.display || getComputedStyle(selector).display;
+}
+/*
+var displayState;
+if(!!selector.style.display){ / !!는 값이 없음을 도출하려 사용했음
+  displayState = getComputedStyle(selector).display;
+}else {
+  displayState = selector.style.display
+}
+*/
+
+// 이벤트 -----------------------------------------------------
+dropBtn.addEventListener('click', function(event){
+  event.preventDefault();
+  var state = displayStateFn(subMenu);
+  console.log(state);
+  (state === 'none')? subMenu.style.display = 'block': subMenu.style.display = 'none';
+})
