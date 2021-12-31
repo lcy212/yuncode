d23d2e0b5a834e4ac15a43019a6bbfdb703ce23e
lcy212 <chaeyoon59@gmail.com>
2021-12-31 09:42:10 +0900

    1230
    포트폴리오

--- b_code/d_project_02_v2/js/src/ideologie.js
+++ b_code/d_project_02_v2/js/src/ideologie.js
@@ -1,11 +1,31 @@
 // ideologie.js
 
-// ideologieBox 시나리오(모바일)
-// .ideologie_inner의 dl에 .on 하면 li의 높이값이 증가하며 dd가 나타남
+// ideologieBox, creedBox 시나리오(모바일)
+// inner의 li를 클릭하면 .on이 삽입된다 / 한 번 더 클릭하면 .on이 삭제된다
+// 다른 li를 선택해도 이전 li에서 .on이 사라지지않는다
 
-// 변수
+
+// 변수 ------------------------------------------------------------------
 const ideoInner = document.querySelector('.ideologie_inner');
 const ideoLi = ideoInner.querySelectorAll('li');
-const ideoLiCvt = [].slice.call(ideoLi); 
+const ideoLiArr = [].slice.call(ideoLi);
 
-// 이벤트
+const creedInner = document.querySelector('.creed_inner');
+const creedLi = creedInner.querySelectorAll('li');
+const creedLiArr = [].slice.call(creedLi);
+
+
+// 함수 ------------------------------------------------------------------
+const fnToggle = (element) => {
+  element.forEach((data, idx) => {
+    data.addEventListener('click', ()=>{
+      data.style.transition = 'all 80ms linear';
+      data.classList.toggle('on');
+    })
+  });
+}
+
+
+// 이벤트처리 ------------------------------------------------------------
+fnToggle(ideoLiArr);
+fnToggle(creedLiArr);
