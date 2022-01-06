78f0d957604a8b55ddf3dba15ccf5d43e5e8e156
lcy212 <chaeyoon59@gmail.com>
2022-01-06 18:25:14 +0900

    0106
    react(router, link)

--- b_code/b_step_06_v2/src/App.js
+++ b_code/b_step_06_v2/src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import Header from './component/Header';
 import Main from './component/Main';
+import Review from './component/Review';
+import CardList from './component/CardList';
 import Footer from './component/Footer';
 import './App.css';
 
+
+
 function App() {
   const title = 'site';
-
   return (
-    <div className="App">
-      <Header heading={title} />
-      <Main />    
-      <Footer heading={title} />
+    <BrowserRouter>
+      <div className="App">
+        <Header heading={title} />
+        <Routes>
+          <Route path="/Main" element={<Main />} />   
+          <Route path="/Review" element={<Review />} />
+          <Route path="/CardList" element={<CardList />} />
+        </Routes>
+        <Footer heading={title} />
     </div>
+    </BrowserRouter>
   );
 }
 
